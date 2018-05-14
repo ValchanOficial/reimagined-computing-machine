2c29a8121c7502b7dc9ffe3b1a5e6cce1c8cd38e
Valeria Padilha de Vargas <valeria_padilha@yahoo.com.br>
2018-05-14 15:52:25 -0300

    testeDeLogin

--- src/main/java/testcase/LoginTestCase.java
+++ src/main/java/testcase/LoginTestCase.java
@@ -3,7 +3,9 @@ package testcase;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -18,16 +20,19 @@ public class LoginTestCase {
 		this.driver = new FirefoxDriver();//ChromeDriver ou qualquer navegador
 		this.driver.get("http://demo.virtuemart.net/");//referencia site a ser testado
 	}
-	
 	@Test
 	public void testMain() {
-		
+		WebElement usernameTextField = this.driver.findElement(By.id("modlgn-username"));
+		WebElement passwordTextField = this.driver.findElement(By.id("modlgn-passwd"));
+		WebElement buttonButton = this.driver.findElement(By.cssSelector("input.button:nth-child(3)"));
+				
+		usernameTextField.sendKeys("demo");
+		passwordTextField.sendKeys("demo");
+		buttonButton.click();
 		
 	}
-	
 	@After
 	public void tearDown() {
 		this.driver.quit();
 	}
-	
 }
