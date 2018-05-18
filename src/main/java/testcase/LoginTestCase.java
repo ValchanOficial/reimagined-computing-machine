ba80264be37336e732c131ab3f6984f73bcaf43e
Valeria Padilha de Vargas <valeria_padilha@yahoo.com.br>
2018-05-18 14:21:11 -0300

    Continuando Testes..

--- src/main/java/testcase/LoginTestCase.java
+++ src/main/java/testcase/LoginTestCase.java
@@ -17,7 +17,7 @@ public class LoginTestCase {
 	
 	@Before
 	public void setUp() {
-		WebDriverManager.firefoxdriver().setup();;//configura para o navegador
+		WebDriverManager.firefoxdriver().setup();//configura para o navegador
 		//baixa dependencias antes do teste
 		this.driver = new FirefoxDriver();//ChromeDriver ou qualquer navegador
 		this.driver.get("http://demo.virtuemart.net/");//referencia site a ser testado
